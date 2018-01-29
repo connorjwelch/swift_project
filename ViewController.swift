744d5f96e41ec61cda5ab01ba8857113f7988515
cjwelch <cjwelch@umich.edu>
2018-01-28 23:06:20 -0500

    Initial swift commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift
+//
+//  Created by Connor Welch on 1/21/18.
+//  Copyright © 2018 Connor Welch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
